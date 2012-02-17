34bb857cdd2fffcf7c86ba3f017bc94df1db084d
John Cadengo <johncadengo@gmail.com>
2012-02-17 12:30:16 -0500

    Generate.

--- generate.sh
+++ generate.sh
@@ -13,13 +13,20 @@ done
 echo "Generating index of the files we've just created."
 ./auto-index.sh ./output > ./output/index.html
 
-echo "Committing it."
+echo "Adding the new files to the git if they haven't already been."
+git add output
+
+echo "Comitting and pushing source to Github."
 git commit -a -m "Generate."
+git push gh source
 
-echo "Pushing it."
+echo "Committing and pushing master to Github."
+cd Feedback
+git commit -a -m "Generate."
 git push origin master
+cd ..
 
-echo "Pulling it remotely."
-ssh cadengo@thedashapp.com 'cd Feedback && git pull'
+#echo "Pulling it to thedashapp.com remotely."
+#ssh cadengo@thedashapp.com 'cd Feedback && git pull'
 
 echo "Done."
