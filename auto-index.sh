396f7ecee314a1535dd27990fb2e4791e3ecb4cd
John Cadengo <johncadengo@gmail.com>
2012-02-17 12:55:05 -0500

    Trying to fix autoindex.

--- auto-index.sh
+++ auto-index.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # usage: auto-index [dir]
 # taken from: http://www.alecjacobson.com/weblog/?p=192
-INDEX=`ls -1 $1/*.html | sed "s/^.*/      <li\>\<a\ href=\"&\"\>&\<\\/a\>\<\\/li\>/"`
+INDEX=`cd $1 && ls -1 *.html | sed "s/^.*/      <li\>\<a\ href=\"&\"\>&\<\\/a\>\<\\/li\>/"`
 echo "<html>
     <head>
         <link href=\"../markdown.css\" rel=\"stylesheet\"></link>
