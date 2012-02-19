aeff5249d84a1785509b117dfde39106662f861c
John Cadengo <johncadengo@gmail.com>
2012-02-19 01:21:02 -0500

    Generate.

---
+++ js/myscripts.js
@@ -0,0 +1,41 @@
+// Stolen from StackOverflow. Find all </code><pre><code> 
+// elements on the page and add the "prettyprint" style. If at least one 
+// prettyprint element was found, call the Google Prettify prettyPrint() API.
+//http://sstatic.net/so/js/master.js?v=6523
+<script type="text/javascript">
+    function styleCode() 
+    {
+        if (typeof disableStyleCode != "undefined") 
+        {
+            return;
+        }
+    
+        var a = false;
+    
+        $("pre code").parent().each(function() 
+        {
+            if (!$(this).hasClass("prettyprint")) 
+            {
+                $(this).addClass("prettyprint");
+                a = true
+            }
+        });
+        
+        if (a) { prettyPrint() } 
+    }
+</script>
+
+// Google analytics
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-17839863-6']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
